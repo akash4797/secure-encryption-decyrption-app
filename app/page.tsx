0d08850193193e5e7433ad1440d50faf397ceae6
Shariar Akash <55664045+akash4797@users.noreply.github.com>
2024-06-26 23:03:33 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import axios from "axios";
 import { cookies } from "next/headers";
 import UserInfo from "./UserInfo";
 import Userlogout from "./Userlogout";
+import { headers } from "next/headers";
 
-const getUser = async (token: string) => {
+const getUser = async (token: string, hostname: string) => {
   try {
-    return await axios.get("http://localhost:3000/api/userinfo", {
+    console.log("my host is:", hostname + "/api/userinfo");
+    return await axios.get(hostname + "/api/userinfo", {
       headers: { Authorization: token },
     });
   } catch {
@@ -14,9 +16,14 @@ const getUser = async (token: string) => {
 };
 
 export default async function Home() {
+  const headerlist = headers();
+  const isDev = process.env.NODE_ENV !== "production";
+  const hostname = isDev
+    ? "http://localhost:3000"
+    : "https://".concat(headerlist.get("x-forwarded-host") || "");
   const c = cookies();
   const token = c.get("access-token")?.value;
-  const userResponse = await getUser(token || "");
+  const userResponse = await getUser(token || "", hostname);
   const user = userResponse?.data.user;
 
   return (
